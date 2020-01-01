a7ffb269db72656ffad5d58bc16318390149a8ad
selenkayan <42153416+selenkayan@users.noreply.github.com>
2020-01-01 12:53:54 +0300

    Delete docker_entrypoint.sh

--- docker_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-exec figlet -c ANN & 
-exec python3.7 ~/breast_cancer_detection/ANN.py &
-exec figlet -c KNN &
-exec python3.7 ~/breast_cancer_detection/KNN.py &
-exec figlet -c SVM & 
-exec python3.7 ~/breast_cancer_detection/SVM.py
