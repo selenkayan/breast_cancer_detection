9591f2efaa6339e50bccb4babfeab4dad13427a5
selenkayan <42153416+selenkayan@users.noreply.github.com>
2020-01-01 13:23:12 +0300

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 figlet -c ANN 
-python3.7 ~/breast_cancer_detection/ANN.py
+python3.7 ~/breast_cancer_detection/ANN.py 2>/dev/null
 figlet -c KNN
-python3.7 ~/breast_cancer_detection/KNN.py
+python3.7 ~/breast_cancer_detection/KNN.py 2>/dev/null
 figlet -c SVM
-python3.7 ~/breast_cancer_detection/SVM.py
+python3.7 ~/breast_cancer_detection/SVM.py 2>/dev/null
